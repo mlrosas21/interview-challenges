4d84654fc6acebeebc7cc836c49d8afe80549a21
Martin Rosas <mlrosas21@hotmail.com>
2023-05-01 20:28:58 -0300

    OK tests ordenar emociones

--- algorithms-7/ordenar-emociones/index.ts
+++ algorithms-7/ordenar-emociones/index.ts
@@ -1,6 +1,22 @@
 import type { Emocion } from "./types";
 
+const PUNTAJES: Record<Emocion, number> = {
+  T_T: 0,
+  ":(": 1,
+  ":|": 2,
+  ":)": 3,
+  ":D": 4
+}
+
 export default function ordenarEmociones(emociones: Emocion[], orden: boolean): Emocion[] {
+
+  return emociones.sort((a,b) => {
+    if(orden){
+      return PUNTAJES[b] - PUNTAJES[a]
+    } else {
+      return PUNTAJES[a] - PUNTAJES[b]
+    }
+  })
   // TODO: implementar
   return [];
 }
