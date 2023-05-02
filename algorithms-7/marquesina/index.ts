1a8c71936fa910c1ad6d5387a73182509791be88
Martin Rosas <mlrosas21@hotmail.com>
2023-05-01 21:54:51 -0300

    OK tests marquesina

--- algorithms-7/marquesina/index.ts
+++ algorithms-7/marquesina/index.ts
@@ -1,4 +1,11 @@
 export default function marquesina(texto: string): string[] {
+  let buffer: string[] = []
+  
+  for(let i=1; i<=texto.length; i++){
+    buffer.push(texto.slice(i)+texto.slice(0, i))
+    console.log(buffer)
+  }
+
   // TODO: implementar
-  return [];
+  return buffer;
 }
