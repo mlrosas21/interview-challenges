c847cfce55072f11ea7f0cdd8af3637f349dd625
Martin Rosas <mlrosas21@hotmail.com>
2023-05-01 22:11:19 -0300

    OK tests gira-sarbalap

--- algorithms-7/gira-sarbalap/index.ts
+++ algorithms-7/gira-sarbalap/index.ts
@@ -1,4 +1,9 @@
 export default function girar(texto: string): string {
-  // TODO: implementar
+  let arr = texto.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length >= 5) arr[i] = arr[i].split("").reverse().join("");
+  }
+  texto = arr.join(" ");
+
   return texto;
 }
