1cba09ac604a15a843d33b494d175063e1ebd807
Martin Rosas <mlrosas21@hotmail.com>
2023-05-03 20:21:22 -0300

    OK test encontrar-al-impar

--- algorithms-7/encontrar-al-impar/index.ts
+++ algorithms-7/encontrar-al-impar/index.ts
@@ -1,4 +1,21 @@
 export default function encontrarImpar(numeros: number[]): number {
-  // TODO: implementar
-  return 0;
+  let registrados = {}
+  let result;
+
+  for(let num of numeros){
+    if(registrados[num]){
+      registrados[num]++
+    } else {
+      registrados[num] = 1
+    }
+  }
+
+  console.log(registrados);
+  
+
+  for(let num in registrados){    
+    if(registrados[num] % 2 !== 0) result = num
+  }
+
+  return parseInt(result)
 }
