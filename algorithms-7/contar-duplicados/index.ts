765b21756e3f69f72bd2c3e8b42a7a980ea659eb
Martin Rosas <mlrosas21@hotmail.com>
2023-05-04 20:34:34 -0300

    OK tests contar-duplicados

--- algorithms-7/contar-duplicados/index.ts
+++ algorithms-7/contar-duplicados/index.ts
@@ -1,4 +1,13 @@
 export default function contarDuplicados(texto: string): number {
-  // TODO: implementar
-  return 0;
+  let contador = new Map
+  const arr = texto.split("").map(e => e.toLowerCase())  
+
+  for(let item of arr){
+    contador[item] = (contador[item] || 0) + 1
+  }   
+
+  const duplicados = Object.entries(contador).reduce((acc, [key, value]) => value >= 2 ? acc+1 : acc, 0
+  );
+
+  return duplicados;
 }
