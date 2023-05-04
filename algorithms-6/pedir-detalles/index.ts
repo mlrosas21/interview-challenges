77e82be79b1fe99ad74f3a9c385e3d0153357330
Martin Rosas <mlrosas21@hotmail.com>
2023-05-04 20:43:58 -0300

    OK tests pedir-detalles

--- algorithms-6/pedir-detalles/index.ts
+++ algorithms-6/pedir-detalles/index.ts
@@ -1,6 +1,14 @@
 import type { Developer } from "./types";
 
 export default function pedirDetalles(developers: Developer[]): Developer[] {
+  developers.forEach(developer => {
+    for(let prop in developer){
+      if(developer[prop] === null){
+        developer.question = `Hi, could you please provide your ${prop}.`
+      }
+    }
+  })
+
   // TODO: implementar
-  return [];
+  return developers.filter(dev => dev.question);
 }
