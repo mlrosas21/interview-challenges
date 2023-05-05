561c23e94d884a97a46329e5719fa1a57b92eafc
Martin Rosas <mlrosas21@hotmail.com>
2023-05-04 22:50:47 -0300

    OK tests validar-parentesis

--- algorithms-5/validar-parentesis/index.ts
+++ algorithms-5/validar-parentesis/index.ts
@@ -1,4 +1,12 @@
 export default function validarParéntesis(parentesis: string): boolean {
-  // TODO: implement
-  return false;
+  let buffer: string[] = [];
+  let arr: string[] = parentesis.split("");
+  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] === ")" && buffer[buffer.length-1] === "("
+      ? buffer.pop()
+      : buffer.push(arr[i]);
+  }
+
+  return buffer.length === 0 ? true : false;
 }
