a86ba06e1902de48aad6922914964f0d6f4c39a5
Martin Rosas <mlrosas21@hotmail.com>
2023-05-04 22:32:55 -0300

    OK tests contar-los-lenguajes

--- algorithms-6/contar-los-lenguajes/index.ts
+++ algorithms-6/contar-los-lenguajes/index.ts
@@ -1,6 +1,11 @@
 import type { Developer } from "./types";
 
 export default function contarLosLenguajes(developers: Developer[]): Record<string, number> {
-  // TODO: implementar
-  return {};
+  let lenguajes = {}
+
+  developers.forEach(developer => {
+    lenguajes[developer.language] = lenguajes[developer.language]+1 || 1 
+  })
+
+  return lenguajes;
 }
