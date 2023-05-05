2773ee3c3a378a88de713d3c7c5f1aaf75caf8eb
Martin Rosas <mlrosas21@hotmail.com>
2023-05-04 22:14:25 -0300

    OK tests encontrar-al-mas-viejo

--- algorithms-6/encontrar-al-mas-viejo/index.ts
+++ algorithms-6/encontrar-al-mas-viejo/index.ts
@@ -1,6 +1,7 @@
 import type { Developer } from "./types";
 
-export default function encontrarAlMasViejo(developers: Developer[]): Developer[] {
-  // TODO: implementar
-  return {};
+export default function encontrarAlMasViejo(developers: Developer[]): Developer[] {  
+  const maxAge = developers.sort((a,b) => b.age - a.age)[0].age  
+  
+  return developers.filter(developer => developer.age === maxAge);
 }
